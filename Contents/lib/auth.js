ac3ff5ed9e1987c09c3fb7dce9113332884d4947
Stéphane ZUCATTI <stephane.zucatti@me.com>
2017-11-07 13:05:11 +0100

    Update auth.js

--- Contents/lib/auth.js
+++ Contents/lib/auth.js
@@ -1,36 +1,23 @@
-module.exports = function(app) {
-	
-	var authom = require("@omneedia/authom");
-	var fs = require('fs');
-	var path = require('path');
-	var sep = "/";
-	
-	var util = require('../util');
-
-	global.Auth = {
+module.exports = function (app, authom, MSettings) {
+	Auth = {
 		user: function (profile, fn) {
-			Auth.login(profile, profile.service,function (response) {
+			console.log(profile);
+			Auth.login(profile, profile.service, function (response) {
 				fn(null, response);
 			});
-		}, 
-		login: function (profile, auth_type, cb) {
+		}
+		, login: function (profile, auth_type, cb) {
 			var off = "Officer";
 			var fs = require('fs');
 			var path = require('path');
-			var Auth = require(global.PROJECT_AUTH + sep +  off + ".js");
+			var Auth = require(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + off + ".js");
 			Auth.using = function (unit) {
-				//built in classes
-				if (unit=="db") return require(global.ROOT+sep+'node_modules'+sep+'@omneedia'+sep+'db'+sep+'lib'+sep+'index.js');
-				try {
-					return require(global.ROOT + sep + 'node_modules' + sep + unit);			
-				} catch(e) {
-					return require(global.PROJECT_BIN + sep + 'node_modules' + sep + unit);
-				};
+				return require(__dirname + path.sep + ".." + path.sep + 'node_modules' + path.sep + unit);
 			};
-			Auth.getProfile = function (user,cb) {
+			Auth.getProfile = function (user, cb) {
 				var response = [];
 				if (cb) {
-					fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json', function(e,r) {
+					fs.readFile(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + 'Profiler.json', function (e, r) {
 						var profiler = JSON.parse(r.toString('utf-8'));
 						for (var el in profiler.profile) {
 							var p = profiler.profile[el];
@@ -38,11 +25,12 @@ module.exports = function(app) {
 						};
 						cb(response);
 					})
-				} else {
+				}
+				else {
 					// DO NOT USE ANYMORE
-					// WILL BE DEPRECATED
-					if (fs.existsSync(global.PROJECT_AUTH + sep + 'Profiler.json')) {
-						var profiler = JSON.parse(require('fs').readFileSync(global.PROJECT_AUTH + sep + 'Profiler.json', 'utf-8'));
+					// DEPRECATED
+					if (fs.existsSync(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + 'Profiler.json')) {
+						var profiler = JSON.parse(require('fs').readFileSync(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + 'Profiler.json', 'utf-8'));
 						for (var el in profiler.profile) {
 							var p = profiler.profile[el];
 							if (p.indexOf(user) > -1) response.push(el);
@@ -53,30 +41,32 @@ module.exports = function(app) {
 			};
 			Auth.login(profile, auth_type, function (response) {
 				cb(response);
-			});			
-		}		
+			});
+		}
 	};
-
+	/*app.get('/login', function (req, res) {
+		res.setHeader('content-type', 'text/html');
+		res.end('{}');
+	});*/
 	app.get('/bye', function (req, res) {
 		res.setHeader('content-type', 'text/html');
 		res.end('<script>top.location.reload(true);window.close();</script>');
 	});
-	
 	app.get('/logout', function (req, res) {
 		req.session.destroy();
 		res.redirect('/bye');
 	});
-	
+
 	function ensureAuthenticated(req, res, next) {
 		if (!req.user) req.user = req.session.user;
 		if (req.user) return next();
 		res.end('{"response":"NOT_LOGIN"}');
-	};	
-	
+	};
 	app.get('/account', ensureAuthenticated, function (req, res) {
+		console.log(req.session);
 		if (!req.user) req.user = req.session.user;
 		var response = [];
-		fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json',function(e,r) {
+		fs.readFile(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + 'Profiler.json', function (e, r) {
 			if (e) return res.end(JSON.stringify(req.user));
 			var profiler = JSON.parse(r.toString('utf-8'));
 			for (var el in profiler.profile) {
@@ -84,16 +74,23 @@ module.exports = function(app) {
 				if (p.indexOf(req.user.mail.split('@')[0]) > -1) response.push(el);
 			};
 			req.user.profiles = response;
-			res.end(JSON.stringify(req.user));			
+			res.end(JSON.stringify(req.user));
 		});
 	});
-	
 	app.post('/account', ensureAuthenticated, function (req, res) {
-    	
+		console.log('*******************');
+		/*if (req.body.udid) {
+                // on récupère le udid crée côté client
+                req.session.udid = new Buffer(req.body.udid, 'base64').toString('utf-8');
+                req.session.device = req.session.udid.split('|')[1];
+                req.session.uid = req.session.udid.split('|')[0];
+                // on ajoute l'utilisateur pour créer le pudid (personal udid)
+                req.session.user.pudid = new Buffer(req.session.uid + '|' + req.session.user.uid + '|' + req.session.device).toString('base64');
+                req.session.udid = req.body.udid;
+        };*/
 		if (!req.user) req.user = req.session.user;
-        var response = [];
-				
-		fs.readFile(global.PROJECT_AUTH + sep + 'Profiler.json',function(e,r) {
+		var response = [];
+		fs.readFile(__dirname + path.sep + ".." + path.sep + "auth" + path.sep + 'Profiler.json', function (e, r) {
 			if (e) return res.end(JSON.stringify(req.user));
 			var profiler = JSON.parse(r.toString('utf-8'));
 			for (var el in profiler.profile) {
@@ -101,32 +98,27 @@ module.exports = function(app) {
 				if (p.indexOf(req.user.mail.split('@')[0]) > -1) response.push(el);
 			};
 			req.user.profiles = response;
-			res.end(JSON.stringify(req.user));			
+			res.end(JSON.stringify(req.user));
 		});
-		
-   	});		
-
-	for (var el in global.settings.auth) {
-		var o=global.settings.auth[el];
-		o.service=el;
-		authom.createServer(o);
+	});
+	for (var el in MSettings.auth) {
+		var config = MSettings.auth[el];
+		config.service = el;
+		authom.createServer(config);
 	};
-
 	authom.on("auth", function (req, res, data) {
 		profile = data;
+		//profile.provider = "google";
 		Auth.user(profile, function (err, response) {
 			req.session.user = response;
-			global.OASocketonAuth(JSON.stringify(response));
+			res.setHeader('content-type', 'text/html');
+			OASocketonAuth(JSON.stringify(response));
 			res.end("<html><body><script>setTimeout(window.close, 1000);</script></body></html>");
 		});
 	});
-	
 	authom.on("error", function (req, res, data) {
 		// called when an error occurs during authentication
-		console.log('-- ERROR ------');
 		console.log(data);
 	});
-	
 	app.get("/auth/:service", authom.app);
-	
 }
